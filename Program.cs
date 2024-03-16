0775f7d2d621ef6be645e6c06735510f365a1e28
Jefferson Veiga <gelsvsampa@hotmail.com>
2024-03-15 22:34:16 -0300

    Aplicando a notificação de evento de mudança de estado na lista de IMC

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
+using AppImc;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using AppImc;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
